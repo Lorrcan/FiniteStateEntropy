047b094de83c23d7a6ffbdffe0f10d3bd808f191
ferreris <g.marrad@gmail.com>
2016-01-09 11:46:53 +0100

    added ZSTD compressor/decompressor

--- programs/commandline.c
+++ programs/commandline.c
@@ -20,13 +20,13 @@
 
   You can contact the author at :
   - Public forum : https://groups.google.com/forum/#!forum/lz4c
-*/
+  */
 /*
   Note : this is stand-alone program.
   It is not part of FSE compression library, just a user program of the FSE library.
   The license of FSE library is BSD.
   The license of this program is GPLv2.
-*/
+  */
 
 
 /***************************************************
@@ -55,7 +55,7 @@
 #  include <fcntl.h>    /* _O_BINARY */
 #  include <io.h>       /* _setmode, _isatty */
 #  ifdef __MINGW32__
-   int _fileno(FILE *stream);   /* MINGW somehow forgets to include this windows declaration into <stdio.h> */
+int _fileno(FILE *stream);   /* MINGW somehow forgets to include this windows declaration into <stdio.h> */
 #  endif
 #  define SET_BINARY_MODE(file) _setmode(_fileno(file), _O_BINARY)
 #  define IS_CONSOLE(stdStream) _isatty(_fileno(stdStream))
@@ -95,163 +95,182 @@ static int   fse_pause = 0;
 ***************************************************/
 static int usage(void)
 {
-    DISPLAY("Usage :\n");
-    DISPLAY("%s [arg] inputFilename [outputFilename]\n", programName);
-    DISPLAY("Arguments :\n");
-    DISPLAY("(default): fse core loop timing tests\n");
-    DISPLAY(" -e : use fse (default)\n");
-    DISPLAY(" -h : use huff0\n");
+	DISPLAY("Usage :\n");
+	DISPLAY("%s [arg] inputFilename [outputFilename]\n", programName);
+	DISPLAY("Arguments :\n");
+	DISPLAY("(default): fse core loop timing tests\n");
+	DISPLAY(" -e : use fse (default)\n");
+	DISPLAY(" -h : use huff0\n");
 	DISPLAY(" -p#: use password to encode\\decode\n");
-    DISPLAY(" -z : use zlib's huffman\n");
+	DISPLAY(" -z : use zlib's huffman\n");
 	DISPLAY(" -s : use ZSTD\n");
-    DISPLAY(" -d : decompression (default for %s extension)\n", FSE_EXTENSION);
-    DISPLAY(" -b : benchmark mode\n");
-    DISPLAY(" -i#: iteration loops [1-9](default : 4), benchmark mode only\n");
-    DISPLAY(" -B#: block size (default : 32768), benchmark mode only\n");
-    DISPLAY(" -H : display help and exit\n");
-    return 0;
+	DISPLAY(" -c : decompression ZSTD\n");
+	DISPLAY(" -d : decompression (default for %s extension)\n", FSE_EXTENSION);
+	DISPLAY(" -b : benchmark mode\n");
+	DISPLAY(" -i#: iteration loops [1-9](default : 4), benchmark mode only\n");
+	DISPLAY(" -B#: block size (default : 32768), benchmark mode only\n");
+	DISPLAY(" -H : display help and exit\n");
+	return 0;
 }
 
 
 static int badusage(void)
 {
-    DISPLAYLEVEL(1, "Incorrect parameters\n");
-    if (displayLevel >= 1) usage();
-    exit(1);
+	DISPLAYLEVEL(1, "Incorrect parameters\n");
+	if (displayLevel >= 1) usage();
+	exit(1);
 }
 
 
 static void waitEnter(void)
 {
-    int unused;
-    DISPLAY("Press enter to continue...\n");
-    unused = getchar();
-    (void)unused;
+	int unused;
+	DISPLAY("Press enter to continue...\n");
+	unused = getchar();
+	(void)unused;
 }
 
 int main(int argc, char** argv)
 {
-    int   i,
-          forceCompress=1, decode=0, bench=0; /* default action if no argument */
-    int   indexFileNames=0;
-    const char* input_filename = NULL;
-    const char* output_filename= NULL;
+	int   i,
+		forceCompress = 1, decode = 0, bench = 0; /* default action if no argument */
+	int   indexFileNames = 0;
+	const char* input_filename = NULL;
+	const char* output_filename = NULL;
 	const char* passwordValue = NULL;
-    char*  tmpFilenameBuffer   = NULL;
-    size_t tmpFilenameSize     = 0;
-    char  extension[] = FSE_EXTENSION;
-    FIO_compressor_t compressor = FIO_fse;
+	char*  tmpFilenameBuffer = NULL;
+	size_t tmpFilenameSize = 0;
+	char  extension[] = FSE_EXTENSION;
+	FIO_compressor_t compressor = FIO_fse;
 
-	//int zstd = 0;
+	const char* dictFileName = NULL;
+	int nextEntryIsDictionary = 0;
+
+	int zstdCompression = 0;
+	int zstdDecompression = 0;
 
 
-    /* Welcome message */
-    programName = argv[0];
-    DISPLAY(WELCOME_MESSAGE);
+	/* Welcome message */
+	programName = argv[0];
+	DISPLAY(WELCOME_MESSAGE);
 
-    if (argc<1) badusage();
+	if (argc < 1) badusage();
 
-    for(i = 1; i <= argc; i++)
-    {
-        char* argument = argv[i];
+	for (i = 1; i <= argc; i++)
+	{
+		char* argument = argv[i];
 
-        if(!argument) continue;   /* Protection if argument empty */
+		if (!argument) continue;   /* Protection if argument empty */
 
-        // Decode command (note : aggregated commands are allowed)
-        if (argument[0]=='-')
-        {
-            // '-' means stdin/stdout
-            if (argument[1]==0)
-            {
-                if (!input_filename) input_filename=stdinmark;
-                else output_filename=stdoutmark;
-            }
+		// Decode command (note : aggregated commands are allowed)
+		if (argument[0] == '-')
+		{
+			// '-' means stdin/stdout
+			if (argument[1] == 0)
+			{
+				if (!input_filename) input_filename = stdinmark;
+				else output_filename = stdoutmark;
+			}
 
-            while (argument[1]!=0)
-            {
-                argument ++;
+			while (argument[1] != 0)
+			{
+				argument++;
 
-                switch(argument[0])
-                {
-                    // Display help
-                case 'V': DISPLAY(WELCOME_MESSAGE); return 0;   // Version
-                case 'H': usage(); return 0;
+				switch (argument[0])
+				{
+					// Display help
+				case 'V': DISPLAY(WELCOME_MESSAGE); return 0;   // Version
+				case 'H': usage(); return 0;
 
-                    // Decoding
-                case 'd': decode=1; bench=0; break;
+					// Decoding
+				case 'd': decode = 1; bench = 0; break;
 
-                    // Benchmark full mode
-                case 'b': bench=1; break;
+					// Benchmark full mode
+				case 'b': bench = 1; break;
 
-                    // fse selection (default)
-                case 'e':
-                    BMK_SetByteCompressor(1);
-                    compressor = FIO_fse;
-                    break;
-				case 's':
-					DISPLAY("ZSTD compression\n");
-					// need to add compression
+					// fse selection (default)
+				case 'e':
+					BMK_SetByteCompressor(1);
+					compressor = FIO_fse;
 					break;
-                    // zstd selection
-                case 'h':
-                    BMK_SetByteCompressor(2);
-                    compressor = FIO_huff0;
-                    break;
+				case 's':
+					DISPLAY("\nZSTD compression\n");
+					// need to add compression
+					zstdCompression = 1;
+					//DISPLAY(zstd);
+					break;
+					// zstd selection
+				case 'c':
+					DISPLAY("\nZSTD decompression\n");
+					decode = 1;
+					zstdDecompression = 1;
+					break;
+				case 'a':
+					DISPLAY("\nZSTD compression\n");
+					// need to add compression
+					//zstd = 1;
+					//DISPLAY(zstd);
+					break;
+					// zstd selection
+				case 'h':
+					BMK_SetByteCompressor(2);
+					compressor = FIO_huff0;
+					break;
 
-                    // zlib mode
-                case 'z':
-                    BMK_SetByteCompressor(3);
-                    compressor = FIO_zlibh;
-                    break;
+					// zlib mode
+				case 'z':
+					BMK_SetByteCompressor(3);
+					compressor = FIO_zlibh;
+					break;
 
-                    // Test
-                case 't': decode=1; output_filename=nulmark; break;
+					// Test
+				case 't': decode = 1; output_filename = nulmark; break;
 
-                    // Overwrite
-                case 'f': FIO_overwriteMode(); break;
+					// Overwrite
+				case 'f': FIO_overwriteMode(); break;
 
-                    // Verbose mode
-                case 'v': displayLevel=4; break;
+					// Verbose mode
+				case 'v': displayLevel = 4; break;
 
-                    // Quiet mode
-                case 'q': displayLevel--; break;
+					// Quiet mode
+				case 'q': displayLevel--; break;
 
-                    // keep source file (default anyway, so useless) (for xz/lzma compatibility)
-                case 'k': break;
+					// keep source file (default anyway, so useless) (for xz/lzma compatibility)
+				case 'k': break;
 
-                    // Modify Block Properties
-                case 'B':
-                    {
-                        unsigned bSize = 0;
-                        while ((argument[1] >='0') && (argument[1] <='9'))
-                        {
-                            unsigned digit = argument[1] - '0';
-                            bSize *= 10;
-                            bSize += digit;
-                            argument++;
-                        }
-                        if (argument[1]=='K') bSize<<=10, argument++;  /* allows using KB notation */
-                        if (argument[1]=='M') bSize<<=20, argument++;
-                        if (argument[1]=='B') argument++;
-                        BMK_SetBlocksize(bSize);
-                    }
-                    break;
+					// Modify Block Properties
+				case 'B':
+				{
+							unsigned bSize = 0;
+							while ((argument[1] >= '0') && (argument[1] <= '9'))
+							{
+								unsigned digit = argument[1] - '0';
+								bSize *= 10;
+								bSize += digit;
+								argument++;
+							}
+							if (argument[1] == 'K') bSize <<= 10, argument++;  /* allows using KB notation */
+							if (argument[1] == 'M') bSize <<= 20, argument++;
+							if (argument[1] == 'B') argument++;
+							BMK_SetBlocksize(bSize);
+				}
+					break;
 
-                    // Modify Stream properties
-                case 'S': break;   // to be completed later
+					// Modify Stream properties
+				case 'S': break;   // to be completed later
 
-                    // Modify Nb Iterations (benchmark only)
-                case 'i':
-                    if ((argument[1] >='1') && (argument[1] <='9'))
-                    {
-                        int iters = argument[1] - '0';
-                        BMK_SetNbIterations(iters);
-                        argument++;
-                    }
-                    break;
+					// Modify Nb Iterations (benchmark only)
+				case 'i':
+					if ((argument[1] >= '1') && (argument[1] <= '9'))
+					{
+						int iters = argument[1] - '0';
+						BMK_SetNbIterations(iters);
+						argument++;
+					}
+					break;
 
-                    // Pause at the end (hidden option)
-                case 'P': fse_pause=1; break;
+					// Pause at the end (hidden option)
+				case 'P': fse_pause = 1; break;
 
 				case 'p':
 					argument++;
@@ -260,100 +279,129 @@ int main(int argc, char** argv)
 					break;
 
 
-                    // Change FSE table size (hidden option)
-                case 'M':
-                    if ((argument[1] >='1') && (argument[1] <='9'))
-                    {
-                        int tableLog = argument[1] - '0';
-                        BMK_SetTableLog(tableLog);
-                        argument++;
-                    }
-                    break;
+					// Change FSE table size (hidden option)
+				case 'M':
+					if ((argument[1] >= '1') && (argument[1] <= '9'))
+					{
+						int tableLog = argument[1] - '0';
+						BMK_SetTableLog(tableLog);
+						argument++;
+					}
+					break;
 
-                    /* Unrecognised command */
-                default : badusage();
-                }
-            }
-            continue;
-        }
+					/* Unrecognised command */
+				default: badusage();
+				}
+			}
+			continue;
+		}
 
-        /* first provided filename is input */
-        if (!input_filename) { input_filename=argument; indexFileNames=i; continue; }
+		/* first provided filename is input */
+		if (!input_filename) { input_filename = argument; indexFileNames = i; continue; }
 
-        /* second provided filename is output */
-        if (!output_filename) { output_filename=argument; continue; }
-    }
+		/* second provided filename is output */
+		if (!output_filename) { output_filename = argument; continue; }
 
-    /* DISPLAYLEVEL(3, WELCOME_MESSAGE); */
+		/* dictionary */
+		if (nextEntryIsDictionary)
+		{
+			nextEntryIsDictionary = 0;
+			dictFileName = argument;
+			continue;
+		}
+	}
 
-    /* No input filename ==> use stdin */
-    if(!input_filename) { input_filename=stdinmark; }
 
-    /* Check if input is defined as console; trigger an error in this case */
-    if (!strcmp(input_filename, stdinmark) && IS_CONSOLE(stdin) ) badusage();
 
-    /* Check if benchmark is selected */
-    if (bench==1) { BMK_benchFiles(argv+indexFileNames, argc-indexFileNames); goto _end; }
-    if (bench==3) { BMK_benchCore_Files(argv+indexFileNames, argc-indexFileNames); goto _end; }   /* no longer possible */
 
-    /* No output filename ==> try to select one automatically (when possible) */
-    while (!output_filename)
-    {
-        if (!IS_CONSOLE(stdout)) { output_filename=stdoutmark; break; }   // Default to stdout whenever possible (i.e. not a console)
-        if ((!decode) && !(forceCompress))   // auto-determine compression or decompression, based on file extension
-        {
-            size_t l = strlen(input_filename);
-            if (!strcmp(input_filename+(l-4), FSE_EXTENSION)) decode=1;
-        }
-        if (!decode)   /* compression to file */
-        {
-            size_t l = strlen(input_filename);
-            if (tmpFilenameSize < l+6) tmpFilenameSize = l+6;
-            tmpFilenameBuffer = (char*)calloc(1,tmpFilenameSize);
-			if (tmpFilenameBuffer==NULL)
+
+	/* DISPLAYLEVEL(3, WELCOME_MESSAGE); */
+
+	/* No input filename ==> use stdin */
+	if (!input_filename) { input_filename = stdinmark; }
+
+	/* Check if input is defined as console; trigger an error in this case */
+	if (!strcmp(input_filename, stdinmark) && IS_CONSOLE(stdin)) badusage();
+
+	/* Check if benchmark is selected */
+	if (bench == 1) { BMK_benchFiles(argv + indexFileNames, argc - indexFileNames); goto _end; }
+	if (bench == 3) { BMK_benchCore_Files(argv + indexFileNames, argc - indexFileNames); goto _end; }   /* no longer possible */
+
+	/* No output filename ==> try to select one automatically (when possible) */
+	while (!output_filename)
+	{
+		if (!IS_CONSOLE(stdout)) { output_filename = stdoutmark; break; }   // Default to stdout whenever possible (i.e. not a console)
+		if ((!decode) && !(forceCompress))   // auto-determine compression or decompression, based on file extension
+		{
+			size_t l = strlen(input_filename);
+			if (!strcmp(input_filename + (l - 4), FSE_EXTENSION)) decode = 1;
+		}
+		if (!decode)   /* compression to file */
+		{
+			size_t l = strlen(input_filename);
+			if (tmpFilenameSize < l + 6) tmpFilenameSize = l + 6;
+			tmpFilenameBuffer = (char*)calloc(1, tmpFilenameSize);
+			if (tmpFilenameBuffer == NULL)
 			{
 				DISPLAY("Not enough memory, exiting ... \n");
 				exit(1);
 			}
-            strcpy(tmpFilenameBuffer, input_filename);
-            strcpy(tmpFilenameBuffer+l, FSE_EXTENSION);
-            output_filename = tmpFilenameBuffer;
-            DISPLAYLEVEL(2, "Compressed filename will be : %s \n", output_filename);
-            break;
-        }
-        /* decompression to file (automatic name will work only if input filename has correct format extension) */
-        {
-            size_t outl;
-            size_t inl = strlen(input_filename);
-            if (tmpFilenameSize < inl+2) tmpFilenameSize = inl+2;
-            tmpFilenameBuffer = (char*)calloc(1,tmpFilenameSize);
-            strcpy(tmpFilenameBuffer, input_filename);
-            outl = inl;
-            if (inl>4)
-                while ((outl >= inl-4) && (input_filename[outl] ==  extension[outl-inl+4])) tmpFilenameBuffer[outl--]=0;
-            if (outl != inl-5) { DISPLAYLEVEL(1, "Cannot determine an output filename\n"); badusage(); }
-            output_filename = tmpFilenameBuffer;
-            DISPLAYLEVEL(2, "Decoding into filename : %s \n", output_filename);
-        }
-    } 
+			strcpy(tmpFilenameBuffer, input_filename);
+			strcpy(tmpFilenameBuffer + l, FSE_EXTENSION);
+			output_filename = tmpFilenameBuffer;
+			DISPLAYLEVEL(2, "Compressed filename will be : %s \n", output_filename);
+			break;
+		}
+		/* decompression to file (automatic name will work only if input filename has correct format extension) */
+		{
+			size_t outl;
+			size_t inl = strlen(input_filename);
+			if (tmpFilenameSize < inl + 2) tmpFilenameSize = inl + 2;
+			tmpFilenameBuffer = (char*)calloc(1, tmpFilenameSize);
+			strcpy(tmpFilenameBuffer, input_filename);
+			outl = inl;
+			if (inl>4)
+			while ((outl >= inl - 4) && (input_filename[outl] == extension[outl - inl + 4])) tmpFilenameBuffer[outl--] = 0;
+			if (outl != inl - 5) { DISPLAYLEVEL(1, "Cannot determine an output filename\n"); badusage(); }
+			output_filename = tmpFilenameBuffer;
+			DISPLAYLEVEL(2, "Decoding into filename : %s \n", output_filename);
+		}
+	}
 
-    /* No warning message in pure pipe mode (stdin + stdout) */
-    if (!strcmp(input_filename, stdinmark) && !strcmp(output_filename,stdoutmark) && (displayLevel==2)) displayLevel=1;
+	/* No warning message in pure pipe mode (stdin + stdout) */
+	if (!strcmp(input_filename, stdinmark) && !strcmp(output_filename, stdoutmark) && (displayLevel == 2)) displayLevel = 1;
 
-    /* Check if input or output are defined as console; trigger an error in this case */
-    if (!strcmp(input_filename, stdinmark)  && IS_CONSOLE(stdin) ) badusage();
-    if (!strcmp(output_filename,stdoutmark) && IS_CONSOLE(stdout)) badusage();
+	/* Check if input or output are defined as console; trigger an error in this case */
+	if (!strcmp(input_filename, stdinmark) && IS_CONSOLE(stdin)) badusage();
+	if (!strcmp(output_filename, stdoutmark) && IS_CONSOLE(stdout)) badusage();
 
-    if (decode) FIO_decompressFilename(output_filename, input_filename, passwordValue);
-    else
-    {
-        FIO_setCompressor(compressor);
 
-		FIO_compressFilename(output_filename, input_filename, passwordValue);
-    }
+
+	if (decode)
+	{
+		if (zstdDecompression){
+			FIO_decompressZstdFilename(output_filename, input_filename, dictFileName);
+		}
+		else
+			FIO_decompressFilename(output_filename, input_filename, passwordValue);
+
+	}
+	else
+	{
+		FIO_setCompressor(compressor);
+		DISPLAY("WARTOSC zstdCompression = %d\n", zstdCompression);
+		if (zstdCompression == 1){
+			FIO_compressZstdFilename(output_filename, input_filename, dictFileName, 1, passwordValue);
+		}
+		else{
+			//zstd = 0;
+			FIO_compressFilename(output_filename, input_filename, passwordValue);
+		}
+
+	}
 
 _end:
-    if (fse_pause) waitEnter();
-    free(tmpFilenameBuffer);
-    return 0;
+	if (fse_pause) waitEnter();
+	free(tmpFilenameBuffer);
+	return 0;
 }
